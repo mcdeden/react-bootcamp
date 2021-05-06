2adc05761c8c12e0d967eaaecd101ee1ac5eb7ac
Deden ferdiansyah <mcdeden@gmail.com>
2021-05-06 16:50:10 +0800

    handling fetch error

--- src/pages/bootcamp.js
+++ src/pages/bootcamp.js
@@ -4,24 +4,34 @@ import Bloglist from '../components/bloglist';
 const Bootcamp = () => {
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => { //this setTimeout is just for simulation
             fetch('http://localhost:8100/blogs')
             .then(res => {
+                if (!res.ok) {
+                  throw Error('Could not the data from the resource');  
+                } 
                 return res.json();
             })
-            .then((data) => {
+            .then(data => {
                 setBlogs(data);
                 setIsLoading(false);
+                setError(null);
+            })
+            .catch(err => {
+                setIsLoading(false);
+                setError(err.message);
             })
         }, 1000);
     },[]);
  
     return ( 
         <div>
-            {isLoading && <div>Loading...</div>}
-            {blogs && <Bloglist blogs = {blogs} title="All Blogs" />}
+            { error && <div> { error }</div> }
+            { isLoading && <div>Loading...</div> }
+            { blogs && <Bloglist blogs = {blogs} title="All Blogs" /> }
         </div>
      );
 }
