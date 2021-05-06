e6d778ce03eeadb045453490bf48836b0eff80e3
Deden ferdiansyah <mcdeden@gmail.com>
2021-05-06 16:23:44 +0800

    display loading when fetch data

--- src/components/bloglist.js
+++ src/components/bloglist.js
@@ -1,6 +1,4 @@
 const Bloglist = ({blogs, title}) => {
-    // const blogs = props.blogs;
-    // const title = props.title;
 
     return ( 
         <div>
--- src/pages/bootcamp.js
+++ src/pages/bootcamp.js
@@ -3,24 +3,24 @@ import Bloglist from '../components/bloglist';
 
 const Bootcamp = () => {
     const [blogs, setBlogs] = useState(null);
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8100/blogs')
-        .then(res => {
-            return res.json();
-        })
-        .then((data) => {
-            setBlogs(data);
-        })
+        setTimeout(() => { //this setTimeout is just for simulation
+            fetch('http://localhost:8100/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then((data) => {
+                setBlogs(data);
+                setIsLoading(false);
+            })
+        }, 1000);
     },[]);
  
     return ( 
         <div>
+            {isLoading && <div>Loading...</div>}
             {blogs && <Bloglist blogs = {blogs} title="All Blogs" />}
         </div>
      );
