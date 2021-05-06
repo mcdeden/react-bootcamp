21a1a9565985466064ac6efb6e77d73da8cedc5e
Deden ferdiansyah <mcdeden@gmail.com>
2021-05-06 15:54:57 +0800

    use useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Content from './layouts/Content';
+import Footer from './layouts/Footer';
+import Header from './layouts/Header';
+import Sidebar from './layouts/Sidebar';
 
 function App() {
+  // const title = 'Welcome to the blog';
+  // const likes = 50;
+  // const person = { name: 'mcdeden', age: 40 };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <Sidebar />
+      <Content />
+      {/* <Dashboard /> */}
+
+      {/* net ninja bootcamp */}
+      {/* <div className="content-wrapper"> */}
+        {/* <h1>{ title }</h1> */}
+        {/* <p>Liked { likes } times</p> */}
+        {/* <p>{ person.name }</p> */}
+      {/* </div> */}
+      {/* end net ninja bootcamp */}
+
+      <Footer />
     </div>
   );
 }
---
+++ src/layouts/Content.js
@@ -0,0 +1,13 @@
+import Bootcamp from "../pages/bootcamp"
+
+const Content = () => {
+    return ( 
+        <div>
+            <div className="content-wrapper">
+                <Bootcamp/>
+            </div>
+        </div>
+     );
+}
+ 
+export default Content;
